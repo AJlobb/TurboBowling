3f03e1c0e6f63a7f037828fed518d3dba47f70ab
Wesley Whittle <wesleywhittle@hotmail.co.uk>
2022-03-14 14:34:52 +0000

    Input test

--- bowling/src/Pages/Tests/Player.test.js
+++ bowling/src/Pages/Tests/Player.test.js
@@ -1,10 +1,22 @@
-const {
-    player
-} = require('../player');
+import { render, screen } from "@testing-library/react";
+import Player from "../player.js";
 
-describe('Get player name', () => {
-    it('A player can retieve their name', () => {
-        const player = new player();
-        expect(player.getName()).toEqual("alex");
-    });
+// describe('Get player name', () => {
+//     it('A player can retieve their name', () => {
+//         const player = new Player();
+//         expect(player.getName()).toEqual("alex");
+//     });
+// });
+
+describe("Checking input value and testing component", () => {
+  test("Input is valid within file", () => {
+    //Rendering Player Component within the virtual dom
+    render(<Player />);
+
+    //Calling the input in a variable
+    const playerInput = screen.getByRole("textbox");
+
+    //Expecting element to be found within component
+    expect(playerInput).toBeInTheDocument();
+  });
 });
--- bowling/src/Pages/player.js
+++ bowling/src/Pages/player.js
@@ -1,12 +1,12 @@
 import React from "react";
 
 class Player extends React.Component {
-    constructor() {
-        
-    }
     render() {
-        renturn(
-            <div></div>
+        return(
+            <div>
+                <input id="PlayerInput" type='textbox'/>
+                <label htmlFor="PlayerInput"></label>
+            </div>
         )
     }
 }
