bdef3a5ef0f34212df22a6b6a27be0c7063d4205
Wesley Whittle <wesleywhittle@hotmail.co.uk>
2022-03-14 14:36:51 +0000

    Update player.js

--- bowling/src/Pages/player.js
+++ bowling/src/Pages/player.js
@@ -1,11 +1,17 @@
 import React from "react";
 
 class Player extends React.Component {
+    constructor(player) {
+        this.name = player
+    }
     render() {
         return(
             <div>
                 <input id="PlayerInput" type='textbox'/>
                 <label htmlFor="PlayerInput"></label>
+                <button className="player">
+                    {this.props.Player}
+                </button>
             </div>
         )
     }
