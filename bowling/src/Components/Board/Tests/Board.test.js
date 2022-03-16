bf2fb1ca2fd814e3ac088695ed1ff05db45f1f89
Wesley Whittle <wesleywhittle@hotmail.co.uk>
2022-03-16 10:50:55 +0000

    Board Testing

--- bowling/src/Components/Board/Tests/Board.test.js
+++ bowling/src/Components/Board/Tests/Board.test.js
@@ -1,12 +1,207 @@
 import { render, screen } from "@testing-library/react";
 import Board from "../board.js";
 
-describe('Checking Boarling Scorecard', () => {
-    it('checking the scorecard displays', () => {
-        render(<Board />);
+describe("Checking Bowling Scorecard", () => {
+  it("Checks title", () => {
+    //Renders Board.js
+    render(<Board />);
 
-        const table = screen.getByTestId("scorecardTable");
+    //Grabs Title by test-id
+    const title = screen.getByTestId("bowling-header");
 
-        expect(table).toContainHTML('<tr></tr>');
-    });
+    //Checks that it displays text
+    expect(title.textContent).toBe("Bowling Game");
+  });
+  
+  it("Checks ScoreCard", () => {
+    //Renders Board.js
+    render(<Board />);
+
+    //Grabs Table by test-id
+    const table = screen.getByTestId("scorecardTable");
+
+    //Checks that scorecard table renders
+    expect(table).toContainHTML("<tr></tr>");
+  });
+
+  it("Checks ScoreCard body", () => {
+    //Renders Board.js
+    render(<Board />);
+
+    //Grabs Table body by test-id
+    const tableBody = screen.getByTestId("score-tbody");
+
+    //Checks that scorecard body renders
+    expect(tableBody).toContainHTML("<tbody></tbody>");
+  });
+
+  it("Checks Frame 1", () => {
+    //Renders Board.js
+    render(<Board />);
+
+    //Grabs Frame 1 by test-id
+    const frameTh1 = screen.getByTestId("frame1-th");
+    const frameTd1 = screen.getByTestId("frame1-td1");
+    const frameTd2 = screen.getByTestId("frame1-td2");
+    const frameTd3 = screen.getByTestId("frame1-td3");
+
+    //Checks that frame 1 renders
+    expect(frameTh1).toContainHTML("<th></th>");
+    expect(frameTd1).toContainHTML("<td></td>");
+    expect(frameTd2).toContainHTML("<td></td>");
+    expect(frameTd3).toContainHTML("<td></td>");
+  });
+
+  it("Checks Frame 2", () => {
+    //Renders Board.js
+    render(<Board />);
+
+    //Grabs Frame 2 by test-id
+    const frameTh1 = screen.getByTestId("frame2-th");
+    const frameTd1 = screen.getByTestId("frame2-td1");
+    const frameTd2 = screen.getByTestId("frame2-td2");
+    const frameTd3 = screen.getByTestId("frame2-td3");
+
+    //Checks that frame 2 renders
+    expect(frameTh1).toContainHTML("<th></th>");
+    expect(frameTd1).toContainHTML("<td></td>");
+    expect(frameTd2).toContainHTML("<td></td>");
+    expect(frameTd3).toContainHTML("<td></td>");
+  });
+
+  it("Checks Frame 3", () => {
+    //Renders Board.js
+    render(<Board />);
+
+    //Grabs Frame 3 by test-id
+    const frameTh1 = screen.getByTestId("frame3-th");
+    const frameTd1 = screen.getByTestId("frame3-td1");
+    const frameTd2 = screen.getByTestId("frame3-td2");
+    const frameTd3 = screen.getByTestId("frame3-td3");
+
+    //Checks that frame 3 renders
+    expect(frameTh1).toContainHTML("<th></th>");
+    expect(frameTd1).toContainHTML("<td></td>");
+    expect(frameTd2).toContainHTML("<td></td>");
+    expect(frameTd3).toContainHTML("<td></td>");
+  });
+
+  it("Checks Frame 4", () => {
+    //Renders Board.js
+    render(<Board />);
+
+    //Grabs Frame 4 by test-id
+    const frameTh1 = screen.getByTestId("frame4-th");
+    const frameTd1 = screen.getByTestId("frame4-td1");
+    const frameTd2 = screen.getByTestId("frame4-td2");
+    const frameTd3 = screen.getByTestId("frame4-td3");
+
+    //Checks that frame 4 renders
+    expect(frameTh1).toContainHTML("<th></th>");
+    expect(frameTd1).toContainHTML("<td></td>");
+    expect(frameTd2).toContainHTML("<td></td>");
+    expect(frameTd3).toContainHTML("<td></td>");
+  });
+
+  it("Checks Frame 5", () => {
+    //Renders Board.js
+    render(<Board />);
+
+    //Grabs Frame 5 by test-id
+    const frameTh1 = screen.getByTestId("frame5-th");
+    const frameTd1 = screen.getByTestId("frame5-td1");
+    const frameTd2 = screen.getByTestId("frame5-td2");
+    const frameTd3 = screen.getByTestId("frame5-td3");
+
+    //Checks that frame 5 renders
+    expect(frameTh1).toContainHTML("<th></th>");
+    expect(frameTd1).toContainHTML("<td></td>");
+    expect(frameTd2).toContainHTML("<td></td>");
+    expect(frameTd3).toContainHTML("<td></td>");
+  });
+
+  it("Checks Frame 6", () => {
+    //Renders Board.js
+    render(<Board />);
+
+    //Grabs Frame 6 by test-id
+    const frameTh1 = screen.getByTestId("frame6-th");
+    const frameTd1 = screen.getByTestId("frame6-td1");
+    const frameTd2 = screen.getByTestId("frame6-td2");
+    const frameTd3 = screen.getByTestId("frame6-td3");
+
+    //Checks that frame 6 renders
+    expect(frameTh1).toContainHTML("<th></th>");
+    expect(frameTd1).toContainHTML("<td></td>");
+    expect(frameTd2).toContainHTML("<td></td>");
+    expect(frameTd3).toContainHTML("<td></td>");
+  });
+
+  it("Checks Frame 7", () => {
+    //Renders Board.js
+    render(<Board />);
+
+    //Grabs Frame 7 by test-id
+    const frameTh1 = screen.getByTestId("frame7-th");
+    const frameTd1 = screen.getByTestId("frame7-td1");
+    const frameTd2 = screen.getByTestId("frame7-td2");
+    const frameTd3 = screen.getByTestId("frame7-td3");
+
+    //Checks that frame 7 renders
+    expect(frameTh1).toContainHTML("<th></th>");
+    expect(frameTd1).toContainHTML("<td></td>");
+    expect(frameTd2).toContainHTML("<td></td>");
+    expect(frameTd3).toContainHTML("<td></td>");
+  });
+
+  it("Checks Frame 8", () => {
+    //Renders Board.js
+    render(<Board />);
+
+    //Grabs Frame 8 by test-id
+    const frameTh1 = screen.getByTestId("frame8-th");
+    const frameTd1 = screen.getByTestId("frame8-td1");
+    const frameTd2 = screen.getByTestId("frame8-td2");
+    const frameTd3 = screen.getByTestId("frame8-td3");
+
+    //Checks that frame 8 renders
+    expect(frameTh1).toContainHTML("<th></th>");
+    expect(frameTd1).toContainHTML("<td></td>");
+    expect(frameTd2).toContainHTML("<td></td>");
+    expect(frameTd3).toContainHTML("<td></td>");
+  });
+
+  it("Checks Frame 9", () => {
+    //Renders Board.js
+    render(<Board />);
+
+    //Grabs Frame 9 by test-id
+    const frameTh1 = screen.getByTestId("frame9-th");
+    const frameTd1 = screen.getByTestId("frame9-td1");
+    const frameTd2 = screen.getByTestId("frame9-td2");
+    const frameTd3 = screen.getByTestId("frame9-td3");
+
+    //Checks that frame 9 renders
+    expect(frameTh1).toContainHTML("<th></th>");
+    expect(frameTd1).toContainHTML("<td></td>");
+    expect(frameTd2).toContainHTML("<td></td>");
+    expect(frameTd3).toContainHTML("<td></td>");
+  });
+
+  it("Checks Frame 10", () => {
+    //Renders Board.js
+    render(<Board />);
+
+    //Grabs Frame 10 by test-id
+    const frameTh1 = screen.getByTestId("frame10-th");
+    const frameTd1 = screen.getByTestId("frame10-td1");
+    const frameTd2 = screen.getByTestId("frame10-td2");
+    const frameTd3 = screen.getByTestId("frame10-td3");
+
+    //Checks that frame 10 renders
+    expect(frameTh1).toContainHTML("<th></th>");
+    expect(frameTd1).toContainHTML("<td></td>");
+    expect(frameTd2).toContainHTML("<td></td>");
+    expect(frameTd3).toContainHTML("<td></td>");
+  });
 });
--- bowling/src/Components/Board/board.js
+++ bowling/src/Components/Board/board.js
@@ -8,45 +8,45 @@ class Board extends React.Component {
         return (
             <div>
                 <div className='board-Header'>
-                    <h1>Bowling Game</h1>
+                    <h1 data-testid='bowling-header'>Bowling Game</h1>
                 </div>
                 <table id='scorecardTable' className='scorecard' cellPadding='1' cellSpacing='0' data-testid="scorecardTable">
-                    <tbody>
+                    <tbody data-testid='score-tbody'>
                         <tr>
-                            <th colSpan='6'>Frame 1</th>
-                            <th colSpan='6'>Frame 2</th>
-                            <th colSpan='6'>Frame 3</th>
-                            <th colSpan='6'>Frame 4</th>
-                            <th colSpan='6'>Frame 5</th>
-                            <th colSpan='6'>Frame 6</th>
-                            <th colSpan='6'>Frame 7</th>
-                            <th colSpan='6'>Frame 8</th>
-                            <th colSpan='6'>Frame 9</th>
-                            <th colSpan='6'>Frame 10</th>
+                            <th colSpan='6' data-testid='frame1-th'>Frame 1</th>
+                            <th colSpan='6' data-testid='frame2-th'>Frame 2</th>
+                            <th colSpan='6' data-testid='frame3-th'>Frame 3</th>
+                            <th colSpan='6' data-testid='frame4-th'>Frame 4</th>
+                            <th colSpan='6' data-testid='frame5-th'>Frame 5</th>
+                            <th colSpan='6' data-testid='frame6-th'>Frame 6</th>
+                            <th colSpan='6' data-testid='frame7-th'>Frame 7</th>
+                            <th colSpan='6' data-testid='frame8-th'>Frame 8</th>
+                            <th colSpan='6' data-testid='frame9-th'>Frame 9</th>
+                            <th colSpan='6' data-testid='frame10-th'>Frame 10</th>
                         </tr>
                         <tr>
-                            <td colSpan='3'></td><td id="frame1" colSpan='3'></td>
-                            <td colSpan='3'></td><td id="frame2" colSpan='3'></td>
-                            <td colSpan='3'></td><td id="frame3" colSpan='3'></td>
-                            <td colSpan='3'></td><td id="frame4" colSpan='3'></td>
-                            <td colSpan='3'></td><td id="frame5" colSpan='3'></td>
-                            <td colSpan='3'></td><td id="frame6" colSpan='3'></td>
-                            <td colSpan='3'></td><td id="frame7" colSpan='3'></td>
-                            <td colSpan='3'></td><td id="frame8" colSpan='3'></td>
-                            <td colSpan='3'></td><td id="frame9" colSpan='3'></td>
-                            <td colSpan='3'></td><td id="frame10" colSpan='3'></td>
+                            <td colSpan='3' data-testid='frame1-td1'></td><td id="frame1" colSpan='3' data-testid='frame1-td2'></td>
+                            <td colSpan='3' data-testid='frame2-td1'></td><td id="frame2" colSpan='3' data-testid='frame2-td2'></td>
+                            <td colSpan='3' data-testid='frame3-td1'></td><td id="frame3" colSpan='3' data-testid='frame3-td2'></td>
+                            <td colSpan='3' data-testid='frame4-td1'></td><td id="frame4" colSpan='3' data-testid='frame4-td2'></td>
+                            <td colSpan='3' data-testid='frame5-td1'></td><td id="frame5" colSpan='3' data-testid='frame5-td2'></td>
+                            <td colSpan='3' data-testid='frame6-td1'></td><td id="frame6" colSpan='3' data-testid='frame6-td2'></td>
+                            <td colSpan='3' data-testid='frame7-td1'></td><td id="frame7" colSpan='3' data-testid='frame7-td2'></td>
+                            <td colSpan='3' data-testid='frame8-td1'></td><td id="frame8" colSpan='3' data-testid='frame8-td2'></td>
+                            <td colSpan='3' data-testid='frame9-td1'></td><td id="frame9" colSpan='3' data-testid='frame9-td2'></td>
+                            <td colSpan='3' data-testid='frame10-td1'></td><td id="frame10" colSpan='3' data-testid='frame10-td2'></td>
                         </tr>
                         <tr>
-                            <td colSpan='6' id="marker0"></td>
-                            <td colSpan='6' id="marker1"></td>
-                            <td colSpan='6' id="marker2"></td>
-                            <td colSpan='6' id="marker3"></td>
-                            <td colSpan='6' id="marker4"></td>
-                            <td colSpan='6' id="marker5"></td>
-                            <td colSpan='6' id="marker6"></td>
-                            <td colSpan='6' id="marker7"></td>
-                            <td colSpan='6' id="marker8"></td>
-                            <td colSpan='6' id="marker9"></td>
+                            <td colSpan='6' id="marker0" data-testid='frame1-td3'></td>
+                            <td colSpan='6' id="marker1" data-testid='frame2-td3'></td>
+                            <td colSpan='6' id="marker2" data-testid='frame3-td3'></td>
+                            <td colSpan='6' id="marker3" data-testid='frame4-td3'></td>
+                            <td colSpan='6' id="marker4" data-testid='frame5-td3'></td>
+                            <td colSpan='6' id="marker5" data-testid='frame6-td3'></td>
+                            <td colSpan='6' id="marker6" data-testid='frame7-td3'></td>
+                            <td colSpan='6' id="marker7" data-testid='frame8-td3'></td>
+                            <td colSpan='6' id="marker8" data-testid='frame9-td3'></td>
+                            <td colSpan='6' id="marker9" data-testid='frame10-td3'></td>
                         </tr>
                     </tbody>
                 </table>
